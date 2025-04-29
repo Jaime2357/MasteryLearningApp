a1cd0469bddcc52c999961782719c51dc1ae533d
Jaime Elepano <jaime@Jaimes-MacBook-Air-939.local>
2025-04-29 15:15:36 -0700

    Set '/' to route to course selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { logout } from './actions'
+//import React from 'react'
+import { createClient } from '@/utils/supabase/server';
+//import { logout } from './actions'
+import { redirect } from 'next/navigation';
 
 
+async function Page() {
 
-const Page = () => {
-  
-  return (
-    <main>
-      <div>hello</div>
-      <div>
-        <button onClick={logout}>
-          Sign Out
-        </button>
-      </div>
-    </main>
+  const supabase = await createClient();
 
-  )
+  const { data: userData, error: authError } = await supabase.auth.getUser();
+  if (authError || !userData?.user) {
+    redirect('/login');
+    return null; // Prevent further execution
+  }
+  else {
+    redirect('/course-selection');
+    return null; // Prevent further execution
+  }
+
+  // return (
+  //   <main>
+  //     <div>hello</div>
+  //     <div>
+  //       <button onClick={logout}>
+  //         Sign Out
+  //       </button>
+  //     </div>
+  //   </main>
+
+  // )
 }
 
 export default Page
