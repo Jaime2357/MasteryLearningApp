7345e9e0e4db368e081d56b072d5db100895de59
Jaime2357 <jaimeelepano2357@gmail.com>
2025-05-16 06:59:01 -0700

    Temporarily removing indicator for right or wrong password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,7 @@ import { Form, Input, Label, Text, TextField, Button } from "@/components/react-
 import { login } from './actions'
 import Link from "next/link";
 
-interface PageProps {
-  searchParams?: { error?: string | null }
-}
-
-export default async function LoginPage({ searchParams }: PageProps) {
+export default function LoginPage() {
 	return (
 		<main className="flex h-screen bg-lime-50">
 			<div className="m-auto px-16 py-20 rounded-xl border bg-white">
@@ -18,18 +14,18 @@ export default async function LoginPage({ searchParams }: PageProps) {
 						<Text slot="description">
 						</Text>
 					</TextField>
-					{searchParams?.error && (
+					{/* {searchParams?.error && (
 						<p className="text-red-500 text-center mb-2">Please enure your email is verified</p>
-					)}
+					)} */}
 					<TextField type="password" name="password" className={"mt-2"}>
 						<Label className="text-sm">Password</Label>
 						<Input className="w-full border pl-3 py-1 mt-1 rounded-lg outline-lime-300 focus:outline-2 bg-gray-200" />
 						<Text slot="description">
 						</Text>
 					</TextField>
-					{searchParams?.error && (
+					{/* {searchParams?.error && (
 							<p className="text-red-500 text-center mb-2">{searchParams?.error}</p>
-					)}
+					)} */}
 					<Button type="submit" className="w-full border rounded-lg p-2 mt-8 cursor-pointer bg-lime-50 transition ease-in-out hover:scale-105 active:bg-gray-300 outline-lime-300 focus-visible:outline-2">
 						Login
 					</Button>
